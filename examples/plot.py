fb7496e00f96223ba25fdace8bc8d9111d945c71
Fredrik Stromberg <fredrik314@gmail.com>
2025-03-25 17:19:58 +0000

    feat: update examples and plotting functions to work with both Sagemath 10.5 and IPython kernel in the notebook.

--- examples/plot.py
+++ examples/plot.py
@@ -8,6 +8,9 @@ from sage.functions.other import floor, ceil
 from sage.geometry.polyhedron.constructor import Polyhedron
 from sage.plot.all import Graphics, point2d, parametric_plot
 from sage.plot.plot3d.shapes2 import point3d
+from sage.repl.rich_output import get_display_manager
+from sage.repl.rich_output.output_graphics3d import (OutputSceneThreejs as threejs,
+                                                     OutputSceneJmol as jmol)
 from sage.rings.real_mpfr import RR
 from sage.calculus.var import var
 from sage.modules.free_module_element import vector
@@ -28,7 +31,7 @@ def plot_polygon(p: Polyhedron, basis: tuple, action: str = 'show', filename: st
 
     - ``p`` -- polygon
     - ``basis`` -- basis for a lattice (usually given by an ideal)
-    - ``action`` -- string (default 'show') can be 'show' or 'save'
+    - ``action`` -- string (default 'show') can be 'show', 'save', 'return'
     - ``filename`` -- filename to save figure to if action = 'save'
     - `inside_bounds`` -- a list of bounds within which to restrict the lattice points
                             (to e.g. only plot inside the polygon)
@@ -39,26 +42,27 @@ def plot_polygon(p: Polyhedron, basis: tuple, action: str = 'show', filename: st
 
     EXAMPLES::
 
-    sage: from hilbert_modgroup.all import *
-    sage: H1 = HilbertModularGroup(5)
-    sage: K1 = H1.base_ring().number_field()
-    sage: P1 = HilbertPullback(H1)
-    sage: z = UpperHalfPlaneProductElement([0.1+I,0.2+0.5*I])
-    sage: p=P1._candidate_integers_sigma(z,domain='preimage',return_polyhedron=True)
-    sage: norm_args = {'norm_bound': 1.0}
-    sage: plot_polygon(p,K1.fractional_ideal(1).integral_basis(),action='show',**norm_args)
-    <Figure size 640x480 with 1 Axes>
-    sage: x = ZZ['x'].gen()
-    sage: K3.<a> = NumberField(x^3-x^2-2*x+1)
-    sage: H3=HilbertModularGroup(K3)
-    sage: P3=HilbertPullback(H3)
-    sage: z=UpperHalfPlaneProductElement([CC(0,1.0),CC(0,1.0),CC(0,1.0)])
-    sage: p=P3._candidate_integers_sigma(z,domain='polytope', return_polyhedron=True)
-    sage: norm_args = {'norm_bound': P3._bound_for_sigma_norm(z),
-    ....:                   'curve_map': P3.basis_matrix_ideal(), 'norm_plot_factor': 1}
-
-plot_polygon(p,[1,1,1],action='show',ticks=[2,2,2],polygon=False,inside_polygon=True,xmin=-2,xmax=2,
-             **norm_args)
+        sage: from hilbert_modgroup.all import *
+        sage: H1 = HilbertModularGroup(5)
+        sage: K1 = H1.base_ring().number_field()
+        sage: P1 = HilbertPullback(H1)
+        sage: z = UpperHalfPlaneProductElement([0.1+I,0.2+0.5*I])
+        sage: p=P1._candidate_integers_sigma(z,domain='preimage',return_polyhedron=True)
+        sage: norm_args = {'norm_bound': 1.0}
+        sage: plot_polygon(p,K1.fractional_ideal(1).integral_basis(),action='return',**norm_args)
+        Graphics object consisting of 18 graphics primitives
+        sage: x = ZZ['x'].gen()
+        sage: K3.<a> = NumberField(x^3-x^2-2*x+1)
+        sage: H3=HilbertModularGroup(K3)
+        sage: P3=HilbertPullback(H3)
+        sage: z=UpperHalfPlaneProductElement([CC(0,1.0),CC(0,1.0),CC(0,1.0)])
+        sage: p=P3._candidate_integers_sigma(z, domain='polytope', return_polyhedron=True)
+        sage: norm_args = {'norm_bound': P3._bound_for_sigma_norm(z), 'viewer': 'jmol',
+        ....:                   'curve_map': P3.basis_matrix_ideal(), 'norm_plot_factor': 1}
+        sage: G = plot_polygon(p, [1,1,1], action='return', ticks=[2,2,2], polygon=False,
+        ....: inside_polygon=True, xmin=-2, xmax=2, **norm_args)
+        sage: type(G)
+        <class 'sage.plot.plot3d.base.Graphics3dGroup'>
     """
     xmin = options.get('xmin') or floor(min(v[0] for v in p.vertices()))
     xmax = options.get('xmax') or ceil(max(v[0] for v in p.vertices()))
@@ -75,6 +79,10 @@ plot_polygon(p,[1,1,1],action='show',ticks=[2,2,2],polygon=False,inside_polygon=
     plot_options = {'point': False, 'line': 'black', 'fill': (0.9, 0.9, 0.9), 'wireframe': 'black',
                     'polygon': [0.9, 0.9, 0.9]}
     show_options = {'ticks': options.get('ticks', [xmax, ymax])}
+    if n > 2:
+        show_options['camera_position'] = options.get('camera_position', (2.3, 2.4, 2.0))
+    if 'viewer' in options:
+        show_options['viewer'] = options['viewer']
     # Remove unwanted options for the polot
     options = {k: v for k, v in options.items() if k in plot_options}
     if hasattr(basis[0], 'complex_embeddings'):
@@ -87,13 +95,13 @@ plot_polygon(p,[1,1,1],action='show',ticks=[2,2,2],polygon=False,inside_polygon=
     G = Graphics()
     G += p.plot(**plot_options)
     if n == 2:
-        add_pts_degree2(G, basis, xmin, xmax, ymin, ymax, inside_polygon, p)
+        G = add_pts_degree2(G, basis, xmin, xmax, ymin, ymax, inside_polygon, p)
         G.set_axes_range(xmin - 1, xmax + 1, ymin - 1, ymax + 1)
         G.axes_labels(labels)
 
     else:
-        add_pts_degree3(G, basis, xmin, xmax, ymin, ymax, zmin, zmax, inside_polygon, curve_map, p,
-                        norm_bound)
+        G = add_pts_degree3(G, basis, xmin, xmax, ymin, ymax, zmin, zmax, inside_polygon,
+                            curve_map, p, norm_bound)
     # Add also the norm bounds for degree 2
     if norm_bound and n == 2:
         B = curve_map or [[1, 0], [0, 1]]
@@ -122,14 +130,65 @@ plot_polygon(p,[1,1,1],action='show',ticks=[2,2,2],polygon=False,inside_polygon=
         G += implicit_plot3d(abs(x1*y1*z1) == norm_bound, (x, xmin, xmax),
                              (y, ymin, ymax), (z, zmin, zmax), color='gray',
                              opacity=0.4, plot_points=100)
-    if action == 'show':
-        return G.matplotlib(**show_options)
+    return return_show(G, action, filename, **show_options)
+
+
+def return_show(G: Graphics, action: str, filename: str, **show_options: P.kwargs):
+    dm = get_display_manager()
+    show_3d = threejs in dm.supported_output() or jmol in dm.supported_output()
+    if action == 'show' and hasattr(G, 'matplotlib'):
+        G.matplotlib(**show_options)
+    if action == 'show' and show_3d:
+        G.show(**show_options)
+    elif action == 'show':
+        return draw_3dplot_as_png(G, **show_options)
     elif action == 'save' and filename:
         G.save(filename, **show_options)
+    elif action == 'return':
+        return G
+    else:
+        raise ValueError(f"action: `{action}` is not a valid value.")
+
+
+def draw_3dplot_as_png(G: Graphics, **show_options: P.kwargs):
+    """
+    Use function taken from sage_docbuild/conf.py to plot 3d plot as an image
+    when no appropriate backend is available.
+
+    EXAMPLES::
+
+        sage: from hilbert_modgroup.all import *
+        sage: x = ZZ['x'].gen()
+        sage: K3.<a> = NumberField(x^3-x^2-2*x+1)
+        sage: H3=HilbertModularGroup(K3)
+        sage: P3=HilbertPullback(H3)
+        sage: z=UpperHalfPlaneProductElement([CC(0,1.0),CC(0,1.0),CC(0,1.0)])
+        sage: p=P3._candidate_integers_sigma(z,domain='polytope', return_polyhedron=True)
+        sage: norm_args = {'norm_bound': P3._bound_for_sigma_norm(z),
+        ....:                   'curve_map': P3.basis_matrix_ideal(), 'norm_plot_factor': 1}
+        sage: G = plot_polygon(p, [1,1,1], action='return', ticks=[2,2,2], polygon=False,
+        ....: inside_polygon=True, xmin=-2, xmax=2, **norm_args) # implicit doctest
+        sage: type(G)
+        <class 'sage.plot.plot3d.base.Graphics3dGroup'>
+    """
+    import matplotlib as mpl
+    import matplotlib.image as mpimg
+    import matplotlib.pyplot as plt
+    from sage.misc.temporary_file import tmp_filename
+    mpl.rcParams['image.interpolation'] = 'bilinear'
+    mpl.rcParams['image.resample'] = False
+    mpl.rcParams['figure.figsize'] = [8.0, 6.0]
+    mpl.rcParams['figure.dpi'] = 80
+    mpl.rcParams['savefig.dpi'] = 100
+    fn = tmp_filename(ext=".png")
+    G.save(fn, **show_options)
+    img = mpimg.imread(fn)
+    show_options.pop('ticks')
+    plt.imshow(img)
 
 
 def add_pts_degree2(G: Graphics, basis: tuple, xmin: float, xmax: float, ymin: float, ymax: float,
-                    inside_polygon: bool, p: Polyhedron) -> None:
+                    inside_polygon: bool, p: Polyhedron) -> Graphics:
     """
 
         Add points to the plot G for the case of degree 2.
@@ -165,7 +224,7 @@ def add_pts_degree2(G: Graphics, basis: tuple, xmin: float, xmax: float, ymin: f
             sage: ymax = 1
             sage: inside_polygon = False
             sage: add_pts_degree2(G, basis, xmin, xmax, ymin, ymax, inside_polygon, p)
-
+            Graphics object consisting of 2 graphics primitives
 
         """
 
@@ -185,11 +244,13 @@ def add_pts_degree2(G: Graphics, basis: tuple, xmin: float, xmax: float, ymin: f
             if inside_polygon and coordinates[0] not in p:
                 continue
             G += point2d(coordinates, zorder=1, color='black', size=20)
+    return G
 
 
 def add_pts_degree3(G, basis: tuple, xmin: float, xmax: float, ymin: float, ymax: float,
                     zmin: float, zmax: float, inside_polygon: bool = False,
-                    curve_map: tuple = None, p: Polyhedron = None, norm_bound: float = 1.0):
+                    curve_map: tuple = None, p: Polyhedron = None,
+                    norm_bound: float = 1.0) -> Graphics:
     """
     Add points to the plot G for the case of degree 3
 
@@ -227,8 +288,9 @@ def add_pts_degree3(G, basis: tuple, xmin: float, xmax: float, ymin: float, ymax
             sage: zmax = 1
             sage: inside_polygon = False
             sage: curve_map = None
-            sage: add_pts_degree3(G, basis, xmin, xmax, ymin, ymax, zmin, zmax)
-
+            sage: G = add_pts_degree3(G, basis, xmin, xmax, ymin, ymax, zmin, zmax)
+            sage: type(G)
+            <class 'sage.plot.plot3d.base.Graphics3dGroup'>
     """
     a, b, c = basis
     coordinate_as_tuple = not hasattr(a, 'complex_embeddings')
@@ -259,3 +321,4 @@ def add_pts_degree3(G, basis: tuple, xmin: float, xmax: float, ymin: float, ymax
             G += point3d([coordinates], zorder=1, color='gray', size=30, opacity=0.75)
         else:
             G += point3d([coordinates], zorder=3, color='black', size=30)
+    return G
